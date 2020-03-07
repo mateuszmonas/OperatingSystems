2b6e13d6988b8571f8801ab235116fd9245e6066
mateuszmonas <mateuszmonas@gmail.com>
2020-03-07 17:34:05 +0100

    zad2

--- 01. Memory management. Libraries. Time measurement/main.c
+++ 01. Memory management. Libraries. Time measurement/main.c
@@ -2,24 +2,24 @@
 
 int main(int argc, char **argv) {
 
-    if(strcmp(argv[0], "create_table")!=0) {
+    if(strcmp(argv[1], "create_table")!=0) {
         // error create table has to be first arg
     }
-    int size = atoi(argv[1]);
+    int size = atoi(argv[2]);
     struct block_array * array = create_block_array(size);
 
-    for (int i = 2; i < argc; ++i) {
+    for (int i = 3; i < argc; ++i) {
         if(strcmp(argv[i], "compare_pairs")==0){
-            char **file_pairs = &argv[++i];
+            int start = ++i;
             int length = 0;
-            while (strcmp(argv[i], "compare_pairs")!=0 || strcmp(argv[i], "remove_block")!=0 || strcmp(argv[i], "remove_operation")!=0){
+            while (i < argc && (strcmp(argv[i], "compare_pairs")!=0 || strcmp(argv[i], "remove_block")!=0 || strcmp(argv[i], "remove_operation")!=0)){
                 length++;
                 i++;
             }
-            add_file_sequence(array, length, file_pairs);
+            add_file_sequence(array, length, &argv[start]);
             compare_files(array);
-            for (int j = 0; j < length; ++j) {
-                save_block(array, file_pairs[j]);
+            for (int j = start; j < start + length; ++j) {
+                save_block(array, argv[j]);
             };
         }
         else if(strcmp(argv[i], "remove_block")==0){
