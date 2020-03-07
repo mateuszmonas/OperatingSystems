59e6ddaa17660bc70cbdb88e4e0cfac6e05da6f3
mateuszmonas <mateuszmonas@gmail.com>
2020-03-07 18:44:24 +0100

    zad3

--- 01. Memory management. Libraries. Time measurement/main.c
+++ 01. Memory management. Libraries. Time measurement/main.c
@@ -1,6 +1,26 @@
+#include <sys/times.h>
+#include <zconf.h>
 #include "mylib.h"
 
+double timeDifference(clock_t t1, clock_t t2){
+    return ((double)(t2 - t1) / sysconf(_SC_CLK_TCK));
+}
+
+void writeResult(clock_t start, clock_t end, struct tms* t_start, struct tms* t_end){
+    printf("\tREAL_TIME: %fl\n", timeDifference(start,end));
+    printf("\tUSER_TIME: %fl\n", timeDifference(t_start->tms_utime, t_end->tms_utime));
+    printf("\tSYSTEM_TIME: %fl\n", timeDifference(t_start->tms_stime, t_end->tms_stime));
+
+//    fprintf(resultFile, "\tREAL_TIME: %fl\n", timeDifference(start, end));
+//    fprintf(resultFile, "\tUSER_TIME: %fl\n", timeDifference(t_start->tms_utime, t_end->tms_utime));
+//    fprintf(resultFile, "\tSYSTEM_TIME: %fl\n", timeDifference(t_start->tms_stime, t_end->tms_stime));
+}
+
 int main(int argc, char **argv) {
+    clock_t program_start, program_end;
+    struct tms *program_start_tms = calloc(1, sizeof * program_start_tms);
+    struct tms *program_end_tms = calloc(1, sizeof * program_end_tms);
+    program_start = times(program_start_tms);
 
     if(strcmp(argv[1], "create_table")!=0) {
         // error create table has to be first arg
@@ -31,6 +51,7 @@ int main(int argc, char **argv) {
             remove_operation(array, atoi(argv[block_to_delete_from]), atoi(argv[operation_to_delete]));
         }
     }
-
+    program_end = times(program_end_tms);
+    writeResult(program_start, program_end, program_start_tms, program_end_tms);
     return 0;
 }
