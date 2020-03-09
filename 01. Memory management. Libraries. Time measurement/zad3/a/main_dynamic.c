501747a9a765fa39ad2a75fbed7a6420de792f00
mateuszmonas <mateuszmonas@gmail.com>
2020-03-09 22:45:06 +0100

    test

--- 01. Memory management. Libraries. Time measurement/zad3/a/main_dynamic.c
+++ 01. Memory management. Libraries. Time measurement/zad3/a/main_dynamic.c
@@ -7,11 +7,14 @@
 #include "stdlib.h"
 #include "string.h"
 
-void writeResult(FILE* result_file, struct timespec * start_time, struct timespec * end_time, struct rusage *start_usage, struct rusage *end_usage){
+void writeResult(FILE* result_file, char* test_title, struct timespec * start_time, struct timespec * end_time, struct rusage *start_usage, struct rusage *end_usage){
+    printf("%s\n", test_title);
     printf("REAL_TIME: %ldns\n", end_time->tv_nsec - start_time->tv_nsec);
     printf("USER_TIME: %ldµs\n", end_usage->ru_utime.tv_usec - start_usage->ru_utime.tv_usec);
     printf("SYSTEM_TIME: %ldµs\n", end_usage->ru_stime.tv_usec - start_usage->ru_stime.tv_usec);
 
+
+    fprintf(result_file, "%s\n", test_title);
     fprintf(result_file, "REAL_TIME: %ldns\n", end_time->tv_nsec - start_time->tv_nsec);
     fprintf(result_file, "USER_TIME: %ldµs\n", end_usage->ru_utime.tv_usec - start_usage->ru_utime.tv_usec);
     fprintf(result_file, "SYSTEM_TIME: %ldµs\n", end_usage->ru_stime.tv_usec - start_usage->ru_stime.tv_usec);
@@ -21,6 +24,7 @@ int main(int argc, char **argv) {
     char *cwd = calloc(PATH_MAX, sizeof(char));
     getcwd(cwd, PATH_MAX);
     char *path = calloc(256, sizeof(char));
+    char *test_title;
     snprintf(path, 256, "%s/%s", cwd, "raport3a.txt");
     FILE *result_file = fopen(path, "a");
     memset(path, 0, 256);
@@ -29,8 +33,6 @@ int main(int argc, char **argv) {
     struct rusage *end_usage = calloc(1, sizeof * end_usage);
     struct timespec *start_time = calloc(1, sizeof *start_time);
     struct timespec *end_time = calloc(1, sizeof *end_time);
-    clock_gettime(CLOCK_REALTIME, start_time);
-    getrusage(RUSAGE_SELF, start_usage);
 
     snprintf(path, 256, "%s/%s", cwd, "../../zad1/out/libmylib.so");
     void * mylib = dlopen(path, RTLD_LAZY);
@@ -42,6 +44,7 @@ int main(int argc, char **argv) {
     unsigned int (*remove_operation)(struct block_array *, int, int) = (unsigned int (*)(struct block_array *, int, int)) dlsym(mylib, "remove_operation");
     if(strcmp(argv[1], "create_table")!=0) {
         // error create table has to be first arg
+        return 1;
     }
     int size = atoi(argv[2]);
     struct block_array * array = create_block_array(size);
@@ -49,16 +52,19 @@ int main(int argc, char **argv) {
     for (int i = 3; i < argc; ++i) {
         if(strcmp(argv[i], "compare_pairs")==0){
             int start = ++i;
-            int length = 0;
-            while (i < argc && (strcmp(argv[i], "save_block")!=0 || strcmp(argv[i], "compare_pairs")!=0 || strcmp(argv[i], "remove_block")!=0 || strcmp(argv[i], "remove_operation")!=0)){
+            int length = 1;
+            while (i + 1 < argc &&
+                   strcmp(argv[i + 1], "save_block") != 0 &&
+                   strcmp(argv[i + 1], "compare_pairs") != 0 &&
+                   strcmp(argv[i + 1], "remove_block") != 0 &&
+                   strcmp(argv[i + 1], "start_time") != 0 &&
+                   strcmp(argv[i + 1], "stop_time") != 0 &&
+                   strcmp(argv[i + 1], "remove_operation") != 0) {
                 length++;
                 i++;
             }
             add_file_sequence(array, length, &argv[start]);
             compare_files(array);
-            for (int j = start; j < start + length; ++j) {
-                save_block(array, argv[j]);
-            }
         }
         else if(strcmp(argv[i], "remove_block")==0){
             remove_block(array, atoi(argv[++i]));
@@ -71,10 +77,17 @@ int main(int argc, char **argv) {
             int operation_to_delete = ++i;
             remove_operation(array, atoi(argv[block_to_delete_from]), atoi(argv[operation_to_delete]));
         }
+        else if(strcmp(argv[i], "start_time")==0){
+            test_title = argv[++i];
+            clock_gettime(CLOCK_REALTIME, start_time);
+            getrusage(RUSAGE_SELF, start_usage);
+        }
+        else if(strcmp(argv[i], "stop_time")==0){
+            clock_gettime(CLOCK_REALTIME, end_time);
+            getrusage(RUSAGE_SELF, end_usage);
+            writeResult(result_file, test_title, start_time, end_time, start_usage, end_usage);
+        }
     }
-    clock_gettime(CLOCK_REALTIME, end_time);
-    getrusage(RUSAGE_SELF, end_usage);
-    writeResult(result_file, start_time, end_time, start_usage, end_usage);
     dlclose(mylib);
     fclose(result_file);
     return 0;
