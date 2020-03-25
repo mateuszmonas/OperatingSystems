bb82a0c9134d1fd1fdd1be7cde8e0fe9efb8e9c4
mateuszmonas <mateuszmonas@gmail.com>
2020-03-25 17:25:55 +0100

    shared file

--- 03. Creating processes. Process environment, process control/zad2/main.c
+++ 03. Creating processes. Process environment, process control/zad2/main.c
@@ -2,10 +2,39 @@
 #include <stdio.h>
 #include <zconf.h>
 #include <wait.h>
-#include <errno.h>
 
 #include "matrix_multiplier.c"
 
+void create_output_files(char* input_file_name){
+    FILE *input_file = fopen(input_file_name, "r");
+    char *line = calloc(256, sizeof(char));
+    while (fgets(line, 256, input_file)) {
+        FILE* A = fopen(strtok(line, " "), "r");
+        FILE* B = fopen(strtok(NULL, " "), "r");
+        FILE* output_file = fopen(strtok(NULL, " "), "w");
+        int rows = 0;
+        int cols = 0;
+        char *matrix_line = calloc(2048, sizeof(char));
+        while(fgets(matrix_line, 2048, A)) rows++;
+        fgets(matrix_line, 2048, B);
+        strtok(matrix_line, " ");
+        while(strtok(NULL, " ")) cols++;
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                fprintf(output_file, "0 ");
+            }
+            fprintf(output_file, "\n");
+        }
+
+        free(matrix_line);
+        fclose(A);
+        fclose(B);
+        fclose(output_file);
+    }
+    free(line);
+    fclose(input_file);
+}
 
 int main(int argc, char** argv) {
 //    if(argc < 4) {
@@ -19,14 +48,18 @@ int main(int argc, char** argv) {
     char* input_file_name = "matrices";
     long process_count = 3;
     long time = 1;
-    bool shared_result_files = false;
+    bool shared_result_files = true;
+
+    if(shared_result_files){
+        create_output_files(input_file_name);
+    }
 
     for (int i = 0; i < process_count; ++i) {
         int p;
         if((p = fork()) == 0){
-            multiply(input_file_name, i, process_count, time);
+            if(shared_result_files) multiply_shared_output_file(input_file_name, i, process_count, time);
+            else multiply_separate_output_files(input_file_name, i, process_count, time);
             return 0;
-            break;
         } else{
             printf("starting process %d\n", p);
         }
--- 03. Creating processes. Process environment, process control/zad2/matrix_multiplier.c
+++ 03. Creating processes. Process environment, process control/zad2/matrix_multiplier.c
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/file.h>
 
 int start_column;
 int columns_to_multiply;
-int rows;
+int matrix_b_rows;
 int multiplication_count = 0;
 
 static void finish_process(){
@@ -15,7 +16,7 @@ static void finish_process(){
 }
 
 void set_number_of_columns(FILE* file, int process_number, int process_count){
-    rows = 1;
+    matrix_b_rows = 1;
     size_t size = 0;
     char *lineptr = NULL;
     int columns_count = 0;
@@ -24,7 +25,7 @@ void set_number_of_columns(FILE* file, int process_number, int process_count){
         columns_count += (lineptr[i] == ' ');
     }
     while (getline(&lineptr, &size, file) != -1){
-        rows++;
+        matrix_b_rows++;
     }
     start_column = columns_count / process_count * process_number;
     columns_to_multiply = process_number == process_count - 1 ? columns_count - start_column : columns_count / process_count;
@@ -32,13 +33,131 @@ void set_number_of_columns(FILE* file, int process_number, int process_count){
     fseek(file, 0, SEEK_SET);
 }
 
-void multiply(char* input_file_name, int process_number, int process_count, long time){
+void multiply_shared_output_file(char* input_file_name, int process_number, int process_count, long time){
     signal(SIGALRM, finish_process);
     alarm(time);
     FILE* input_file = fopen(input_file_name, "r");
-    char *line = calloc(256, sizeof(char));
+    char *line = calloc(2048, sizeof(char));
 
-    while (fgets(line, 256, input_file)){
+    while (fgets(line, 2048, input_file)){
+        char *matrix_a_file_name = strtok(line, " ");
+        char *matrix_b_file_name = strtok(NULL, " ");;
+        char *out_file_name = strtok(NULL, " ");
+
+        FILE* output_file = fopen(out_file_name, "r+");
+        FILE *matrix_a = fopen(matrix_a_file_name, "r");
+        FILE *matrix_b = fopen(matrix_b_file_name, "r");
+
+        set_number_of_columns(matrix_b, process_number, process_count);
+
+        int rows = 0;
+        int cols = 0;
+        char *matrix_line = calloc(2048, sizeof(char));
+        while(fgets(matrix_line, 2048, matrix_a)) rows++;
+        fgets(matrix_line, 2048, matrix_b);
+        strtok(matrix_line, " ");
+        while(strtok(NULL, " ")) cols++;
+        long **result_matrix = calloc(rows, sizeof(long *));
+        for (int l = 0; l < rows; ++l) {
+            result_matrix[l] = calloc(cols, sizeof(long));
+        }
+        fseek(matrix_a, 0, SEEK_SET);
+        fseek(matrix_b, 0, SEEK_SET);
+
+        char *matrix_row_a = calloc(2048, sizeof(char));
+        char *matrix_row_b = calloc(2048, sizeof(char));
+        int current_row = 0;
+        while (fgets(matrix_row_a, 2048, matrix_a)){
+            int *row_matrix_a_nums = calloc(matrix_b_rows, sizeof(int));
+            row_matrix_a_nums[0] = strtol(strtok(matrix_row_a, " "), NULL, 10);
+            for (int j = 1; j < matrix_b_rows; ++j) {
+                row_matrix_a_nums[j] = strtol(strtok(NULL, " "), NULL, 10);
+            }
+
+            long result;
+            char *current_num1;
+            for (int current_column = 0; current_column < columns_to_multiply; ++current_column) {
+                fseek(matrix_b, 0, SEEK_SET);
+
+                // read correct column from b matrix
+                fgets(matrix_row_b, 2048, matrix_b);
+                current_num1 = strtok(matrix_row_b, " ");
+                for (int i = 0; i < start_column + current_column; ++i) {
+                    current_num1 = strtok(NULL, " ");
+                }
+
+                result = row_matrix_a_nums[0] * strtol(current_num1, NULL, 10);
+
+                for (int current_row = 1; current_row < matrix_b_rows; ++current_row) {
+                    fgets(matrix_row_b, 2048, matrix_b);
+                    current_num1 = strtok(matrix_row_b, " ");
+                    for (int i = 0; i < start_column + current_column; ++i) {
+                        current_num1 = strtok(NULL, " ");
+                    }
+                    result += row_matrix_a_nums[current_row] * strtol(current_num1, NULL, 10);
+                }
+                result_matrix[current_row][start_column + current_column] = result;
+            }
+            current_row++;
+            free(row_matrix_a_nums);
+        }
+        int fd = fileno(output_file);
+        flock(fd, LOCK_EX);
+
+
+        for (int k = 0; k < rows; ++k) {
+            fgets(matrix_line, 2048, output_file);
+            long temp = strtol(strtok(matrix_line, " "), NULL, 10);
+            result_matrix[k][0] = start_column == 0 ? result_matrix[k][0] : temp;
+            for (int i = 1; i < cols; ++i) {
+                if (start_column <= i && i < start_column + columns_to_multiply) continue;
+                result_matrix[k][i] = strtol(strtok(NULL, " "), NULL, 10);
+            }
+        }
+
+        for (int k = 0; k < rows; ++k) {
+            for (int i = 0; i < cols; ++i) {
+                printf("%ld ", result_matrix[k][i]);
+            }
+            printf("\n");
+        }
+
+        ftruncate(fd, 0);
+        fseek(output_file, 0, SEEK_SET);
+        for (int k = 0; k < rows; ++k) {
+            for (int i = 0; i < cols; ++i) {
+                fprintf(output_file, "%ld ", result_matrix[k][i]);
+            }
+            fprintf(output_file, "\n");
+        }
+
+        flock(fd, LOCK_UN);
+
+
+        for (int l = 0; l < cols; ++l) {
+            free(result_matrix[l]);
+        }
+        free(result_matrix);
+        free(matrix_line);
+        fclose(matrix_a);
+        fclose(matrix_b);
+        fclose(output_file);
+        free(matrix_row_a);
+        free(matrix_row_b);
+        multiplication_count++;
+    }
+    free(line);
+    fclose(input_file);
+    while (true) {}
+}
+
+void multiply_separate_output_files(char* input_file_name, int process_number, int process_count, long time){
+    signal(SIGALRM, finish_process);
+    alarm(time);
+    FILE* input_file = fopen(input_file_name, "r");
+    char *line = calloc(2048, sizeof(char));
+
+    while (fgets(line, 2048, input_file)){
         char *matrix_a_file_name = strtok(line, " ");
         char *matrix_b_file_name = strtok(NULL, " ");;
         char *out_file_name = calloc(64, sizeof(char));
@@ -52,12 +171,12 @@ void multiply(char* input_file_name, int process_number, int process_count, long
         set_number_of_columns(matrix_b, process_number, process_count);
 
         long *results = calloc(columns_to_multiply, sizeof(long));
-        char *matrix_row_a = calloc(256, sizeof(char));
-        char *matrix_row_b = calloc(256, sizeof(char));
-        while (fgets(matrix_row_a, 256, matrix_a)){
-            int *row_matrix_a_nums = calloc(rows, sizeof(int));
+        char *matrix_row_a = calloc(2048, sizeof(char));
+        char *matrix_row_b = calloc(2048, sizeof(char));
+        while (fgets(matrix_row_a, 2048, matrix_a)){
+            int *row_matrix_a_nums = calloc(matrix_b_rows, sizeof(int));
             row_matrix_a_nums[0] = strtol(strtok(matrix_row_a, " "), NULL, 10);
-            for (int j = 1; j < rows; ++j) {
+            for (int j = 1; j < matrix_b_rows; ++j) {
                 row_matrix_a_nums[j] = strtol(strtok(NULL, " "), NULL, 10);
             }
 
@@ -67,7 +186,7 @@ void multiply(char* input_file_name, int process_number, int process_count, long
                 fseek(matrix_b, 0, SEEK_SET);
 
                 // read correct column from b matrix
-                fgets(matrix_row_b, 256, matrix_b);
+                fgets(matrix_row_b, 2048, matrix_b);
                 current_num1 = strtok(matrix_row_b, " ");
                 for (int i = 0; i < start_column + current_column; ++i) {
                     current_num1 = strtok(NULL, " ");
@@ -75,8 +194,8 @@ void multiply(char* input_file_name, int process_number, int process_count, long
 
                 result = row_matrix_a_nums[0] * strtol(current_num1, NULL, 10);
 
-                for (int current_row = 1; current_row < rows; ++current_row) {
-                    fgets(matrix_row_b, 256, matrix_b);
+                for (int current_row = 1; current_row < matrix_b_rows; ++current_row) {
+                    fgets(matrix_row_b, 2048, matrix_b);
                     current_num1 = strtok(matrix_row_b, " ");
                     for (int i = 0; i < start_column + current_column; ++i) {
                         current_num1 = strtok(NULL, " ");
@@ -88,10 +207,8 @@ void multiply(char* input_file_name, int process_number, int process_count, long
 
             for (int k = 0; k < columns_to_multiply; ++k) {
                 fprintf(output_file, "%ld ", results[k]);
-                printf("%ld ", results[k]);
             }
             fprintf(output_file, "\n");
-            printf("\n");
             free(row_matrix_a_nums);
         }
         fclose(matrix_a);
