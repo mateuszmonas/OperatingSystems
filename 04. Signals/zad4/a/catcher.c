f4882b88144d54b094a3a43d7b787dbc5a8a58ba
mateuszmonas <mateuszmonas@gmail.com>
2020-03-30 11:20:57 +0200

    zad4a

---
+++ 04. Signals/zad4/a/catcher.c
@@ -0,0 +1,84 @@
+#include <string.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define KILL 0
+#define SIGQUEUE 1
+#define SIGRT 2
+
+int received_signals = 0;
+bool waiting_for_signals = true;
+int sender_pid;
+int mode = KILL;
+
+void handle_sigusr1(){
+    received_signals++;
+}
+
+void handle_sigusr2(int sig, siginfo_t * info, void *ucontext){
+    sender_pid = info->si_pid;
+    waiting_for_signals = false;
+}
+
+int main(int argc, char** argv){
+
+    if(strcmp(argv[3], "kill") == 0){
+        mode = KILL;
+    } else if (strcmp(argv[3], "sigqueue") == 0) {
+        mode = SIGQUEUE;
+    } else if (strcmp(argv[3], "sigrt") == 0) {
+        mode = SIGRT;
+    }
+
+    signal(SIGRTMIN, handle_sigusr1);
+    signal(SIGUSR1, handle_sigusr1);
+    struct sigaction action;
+    sigemptyset(&action.sa_mask);
+    action.sa_sigaction = handle_sigusr2;
+    action.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGRTMIN + 1, &action, NULL);
+    sigaction(SIGUSR2, &action, NULL);
+
+    printf("%d\n", getpid());
+
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGUSR1);
+    sigdelset(&mask, SIGUSR2);
+    sigdelset(&mask, SIGRTMIN);
+    sigdelset(&mask, SIGRTMIN + 1);
+    sigdelset(&mask, SIGINT);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
+
+    while (waiting_for_signals) {
+        sigsuspend(&mask);
+    }
+    union sigval value;
+    for (int i = 0; i < received_signals; ++i) {
+        switch(mode){
+            case KILL:
+                kill(sender_pid, SIGUSR1);
+                break;
+            case SIGQUEUE:
+                value.sival_int = i;
+                sigqueue(sender_pid, SIGUSR1, value);
+            case SIGRT:
+                kill(sender_pid, SIGRTMIN + 1);
+                break;
+        }
+    }
+    switch(mode){
+        case KILL:
+            kill(sender_pid, SIGUSR2);
+            break;
+        case SIGQUEUE:
+            value.sival_int = 0;
+            sigqueue(sender_pid, SIGUSR2, value);
+        case SIGRT:
+            kill(sender_pid, SIGRTMIN + 1);
+            break;
+    }
+}
---
+++ 04. Signals/zad4/a/sender.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define KILL 0
+#define SIGQUEUE 1
+#define SIGRT 2
+
+int received_signals = 0;
+bool waiting_for_signals = true;
+bool use_sigqueue = false;
+
+void using_kill(int pid, int amount){
+    for (int i = 0; i < amount; ++i) {
+        kill(pid, SIGUSR1);
+    }
+    kill(pid, SIGUSR2);
+}
+
+void using_sigqueue(int pid, int amount){
+    union sigval value;
+    value.sival_int = 0;
+    for (int i = 0; i < amount; ++i) {
+        value.sival_int = i;
+        sigqueue(pid, SIGUSR1, value);
+    }
+    sigqueue(pid, SIGUSR2, value);
+}
+
+void using_sigrt(int pid, int amount) {
+    for (int i = 0; i < amount; ++i) {
+        kill(pid, SIGRTMIN);
+    }
+    kill(pid, SIGRTMIN + 1);
+}
+
+void handle_sigusr1(int sig, siginfo_t * info, void *ucontext){
+    if(use_sigqueue){
+        printf("otrzymano sygnał %d\n", info->si_value.sival_int);
+    }
+    received_signals++;
+}
+
+void handle_sigusr2(){
+    waiting_for_signals = false;
+}
+
+int main(int argc, char** argv){
+    if(argc < 4){
+        fprintf(stderr, "not enough arguments\n");
+        return 1;
+    }
+    int pid = (int) strtol(argv[1], NULL, 10);
+    int message_count = (int) strtol(argv[2], NULL, 10);
+    if(strcmp(argv[3], "kill") == 0){
+        using_kill(pid, message_count);
+    } else if (strcmp(argv[3], "sigqueue") == 0) {
+        use_sigqueue = true;
+        using_sigqueue(pid, message_count);
+    } else if (strcmp(argv[3], "sigrt") == 0) {
+        using_sigrt(pid, message_count);
+    }
+    struct sigaction action;
+    sigemptyset(&action.sa_mask);
+    action.sa_sigaction = handle_sigusr1;
+    action.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGUSR1, &action, NULL);
+    signal(SIGUSR2, handle_sigusr2);
+
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, SIGUSR1);
+    sigdelset(&mask, SIGUSR2);
+    sigdelset(&mask, SIGRTMIN);
+    sigdelset(&mask, SIGRTMIN + 1);
+    sigdelset(&mask, SIGINT);
+    while (waiting_for_signals) {
+        sigsuspend(&mask);
+    }
+    printf("otrzymane sygnały: %d/%d\n", received_signals, message_count);
+}
