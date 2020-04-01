5462ed5e2fc1a50adb2314d86b2aac38fd050e65
mateuszmonas <mateuszmonas@gmail.com>
2020-04-01 16:27:14 +0200

    zad3

--- 04. Signals/zad3/main.c
+++ 04. Signals/zad3/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <time.h>
 
 bool waiting_for_signal = true;
 
@@ -15,7 +16,8 @@ void handle_sigchld(int sig, siginfo_t * info, void *ucontext){
 
 void handle_sigalarm(int sig, siginfo_t * info, void *ucontext){
     printf("sigalarm\n");
-    printf("si_timerid: %d\n", info->si_timerid);
+    printf("si_code: %d == %d :SI_TIMER\n", info->si_code, SI_TIMER);
+    printf("si_timerid: %d si_overrun: %d\n", info->si_timerid, info->si_overrun);
     waiting_for_signal = false;
 }
 
@@ -48,8 +50,22 @@ int main(int argc, char** argv){
         sigqueue(getpid(), SIGUSR1, value);
     } else if(strcmp(argv[1], "sigalarm") == 0){
         act.sa_sigaction = handle_sigalarm;
-        sigaction(SIGALRM, &act, NULL);
-        alarm(1);
+        sigaction(SIGRTMIN, &act, NULL);
+
+        struct itimerspec its;
+        struct sigevent sev;
+        timer_t timerid;
+
+        sev.sigev_notify = SIGEV_SIGNAL;
+        sev.sigev_signo = SIGRTMIN;
+        sev.sigev_value.sival_ptr = &timerid;
+        timer_create(CLOCK_REALTIME, &sev, &timerid);
+        its.it_value.tv_sec = 1000000000 / 1000000000;
+        its.it_value.tv_nsec = 1000000000 % 1000000000;
+        its.it_interval.tv_sec = its.it_value.tv_sec;
+        its.it_interval.tv_nsec = its.it_value.tv_nsec;
+
+        timer_settime(timerid, 0, &its, NULL);
     }
 
     while(waiting_for_signal){}
